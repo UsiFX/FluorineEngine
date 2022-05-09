1f89a95f7147611e6977f6f6c82eb7da609a5901
UsiFX <xprjkt@gmail.com>
2022-05-09 14:30:12 +0200

    fluorine: initialze v0.2
    * lisense changed
    * for now we added some scheduler tweaks
    
    Signed-off-by: UsiFX <xprjkt@gmail.com>

--- fluorine.sh
+++ fluorine.sh
@@ -1,26 +1,27 @@
 #!/system/bin/sh
-# Copyright (C) 2022 The Fluorine Project
 #
-#                    The Linux Foundation
+#   Copyright (C) 2022  xprjkt°
+#   Copyright (C) 2022  Youssef Mahmoud
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 
 #####################
 # Engine Variables
 #####################
 
-fluorineversion="0.01"
+fluorineversion="0.02"
 logpath="/sdcard/.fluorine/atom.log"
 workpath="/sdcard/.fluorine"
 
@@ -105,6 +106,10 @@ gaming()
 	write "${kgsl}/devfreq/adrenoboost" "3"
 	write "/sys/module/cpu_input_boost/parameters/input_boost_duration" 128 	# Extras
 	write "/sys/module/mmc_core/parameters/use_spi_crc" "0" 					# Extras
+	write "/sys/devices/system/cpu/cpufreq/policy4/schedutil/hispeed_load" "60"
+	write "/sys/devices/system/cpu/cpufreq/policy4/schedutil/iowait_boost_enable" "0"
+	write "/sys/devices/system/cpu/cpufreq/policy4/schedutil/down_rate_limit_us" "0"
+	write "/sys/devices/system/cpu/cpufreq/policy4/schedutil/up_rate_limit_us" "0"
 }
 
 #####################
@@ -173,3 +178,11 @@ then
 			*) printmsg -w "Unknown Argument: [ $1 ]"; __print_help ;;
 		esac
 fi
+
+
+
+
+
+
+
+
